8a1bba5c9c0d55c63afdaf7e4f01b2d2a492ac46
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-13 23:53:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,10 @@
 def badge_maker(name)
- puts "Hello, my name is #{name}"
+ "Hello, my name is #{name}"
+ 
 end
 
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    ba
 # Write your code here.
