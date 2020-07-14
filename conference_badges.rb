89c4f8a0ca984900ffc0cf173b01c8cd253e525d
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-14 01:25:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badges|
     puts badges
   end
-  assign_rooms.each(room_assignments) do |rooms|
+  assign_rooms(room_assignments).each do |rooms|
     puts rooms
   end
   printer
