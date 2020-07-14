dcebf3fed3df78977652a9efd586c796493ac559
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-14 00:47:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-   room_assignments = []
-  attendees.each_with_index do |name, index|
+  rooms = []
+  room_assignments.each_with_index do |name, index|
     rooms << "Hello, #{name}! You'll be assigned to room #{index +1}!"
   end
   rooms
